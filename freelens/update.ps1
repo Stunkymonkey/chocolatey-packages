478c3201b829b888d8e5c327041c4e7b6568b50b
Felix Buehler <account@buehler.rocks>
2025-03-16 22:04:18 +0100

    add ChecksumType64

--- freelens/update.ps1
+++ freelens/update.ps1
@@ -5,8 +5,8 @@ $releases = 'https://api.github.com/repos/freelensapp/freelens/releases/latest'
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*Url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
@@ -16,8 +16,9 @@ function global:au_GetLatest {
     $latestVersion = ($assets.Content | ConvertFrom-Json).tag_name -replace "^v", ""
 
     @{
-        URL64   = "https://github.com/freelensapp/freelens/releases/download/v$latestVersion/Freelens-$latestVersion-windows-amd64.msi"
-        Version = $latestVersion
+        URL64          = "https://github.com/freelensapp/freelens/releases/download/v$latestVersion/Freelens-$latestVersion-windows-amd64.msi"
+        Version        = $latestVersion
+        ChecksumType64 = 'sha256'
     }
 }
 
