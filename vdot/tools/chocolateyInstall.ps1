e668c612d6ef430bf144dffc3bfd3d975e415d05
Felix Buehler <account@buehler.rocks>
2025-08-11 23:50:48 +0200

    vdot: init

---
+++ vdot/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$version         = $env:ChocolateyPackageVersion
+$toolsDir        = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installationDir = "$(Get-ToolsLocation)"
+
+$packageArgs = @{
+  PackageName            = "$packageName"
+  Url                    = "https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/refs/tags/$version.zip"
+  Checksum               = 'f766a5a8f2f3e5929549ddf7678b5ecd487be37433ccdd179c5fdf60ccbb08dd'
+  ChecksumType           = 'sha256'
+  UnzipLocation          = "$installationDir"
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ vdot/tools/chocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$installationDir = "$(Get-ToolsLocation)"
+
+Get-ChildItem $installationDir -Directory -Filter "Virtual-Desktop-Optimization-Tool-VDOT-*" | Remove-Item -Recurse -Force
