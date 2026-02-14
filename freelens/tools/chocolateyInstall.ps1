f49aa10d51f7ed0f77be97652a9ad9518c2fe365
David Gardiner <david@gardiner.net.au>
2026-02-14 02:34:50 +0000

    AU-dotnet: 1 updated [skip ci]

--- freelens/tools/chocolateyInstall.ps1
+++ freelens/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   PackageName            = 'freelens'
   FileType               = 'msi'
-  Url64bit               = 'https://github.com/freelensapp/freelens/releases/download/v1.8.0/Freelens-1.8.0-windows-amd64.msi'
-  Checksum64             = 'a059489ff54d2fe2e1c4f301db82da601204bbdb5caa1f7e94cb460222c0cf74'
+  Url64bit               = 'https://github.com/freelensapp/freelens/releases/download/v1.8.1/Freelens-1.8.1-windows-amd64.msi'
+  Checksum64             = 'a65d9c94e66491fde93ab275dad4c6ba1c88005433a7becf5291ed1d92fa6954'
   ChecksumType64         = 'sha256'
   SilentArgs             = '/qn'
   ValidExitCodes         = @(0)
